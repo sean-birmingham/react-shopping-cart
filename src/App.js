d6e76295f0f40fb54b9a1be5f32cf926b9af63e7
Sean Birmingham <seannbirmingham98@gmail.com>
2020-01-29 11:31:48 -0500

    set up CartContext, refactored ShoppingCart and Navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,21 +20,23 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart, setCart }}>
+				<div className="App">
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					component={ShoppingCart} />}
-			/>
+					<Route
+						path="/cart"
+						component={ShoppingCart} />}
+/>
 			</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
