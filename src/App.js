429c3ab97784b64e76ab94e93f63cc2c782e8cba
Sean Birmingham <seannbirmingham98@gmail.com>
2020-01-29 12:29:26 -0500

    set up localStorage for cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import ProductContext from "./contexts/ProductContext";
@@ -11,10 +11,13 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState(() => !JSON.parse(localStorage.getItem('cart')) ? [] : JSON.parse(localStorage.getItem('cart')));
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart]);
 
 	const addItem = item => {
-		// add the given item to the cart
 		setCart([...cart, item])
 	};
 
